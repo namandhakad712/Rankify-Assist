b9ea888b5db7ca6068f6d453e21e0191f967f94b
Naman Dhakad <namandhakad712@gmail.com>
2025-12-17 11:14:14 +0530

    EKO BYE, WELCOME NANOBROWSER BACK

--- extension-RAW/src/background/agent/index.ts
+++
@@ -1,8 +0,0 @@
-import { global } from "@eko-ai/eko";
-import { SimpleChatService } from "./chat-service";
-import { SimpleBrowserService } from "./browser-service";
-
-export function initAgentServices() {
-  global.browserService = new SimpleBrowserService();
-  global.chatService = new SimpleChatService();
-}
--- extension-RAW/src/content/index.ts
+++
@@ -1,5 +0,0 @@
-declare const eko: any;
-declare module "react-markdown";
-declare module "remark-gfm";
-declare module "remark-math";
-declare module "rehype-katex";
